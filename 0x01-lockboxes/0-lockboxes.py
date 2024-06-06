4f84ff4e4752a278b55c5726aa13f372c55a1890
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2024-06-07 00:20:48 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+'''LockBoxes Challenge'''
+
 
 def canUnlockAll(boxes):
-    """Determines if all boxes can be opened."""
-    num_boxes = len(boxes)
-    keys = set([0])
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while True:
-        new_keys = set([key for key in keys for box in [boxes[key]] if box])
-        if not new_keys or new_keys == keys:
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
             break
-        keys = keys.union(new_keys)
 
-    return num_boxes == len(keys)
-
-# Test cases
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # Expected output: True
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # Expected output: True
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))  # Expected output: False
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
